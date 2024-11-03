195ff1c3f2040c7ed7c4476d6f33f0801e4ecb6c
Samir Idris <samzidris11@gmail.com>
2024-11-03 23:24:25 +0100

    update add env variables

--- custom_minting/e2e/custom_minting.ts
+++ custom_minting/e2e/custom_minting.ts
@@ -9,18 +9,20 @@ import {
     resolveScriptHash,
 } from "@meshsdk/core";
 import { builtinByteString, conStr, mConStr0, mConStr1, outputReference, PlutusScript, scriptHash, stringToHex, UTxO } from "@meshsdk/common";
+import dotenv from "dotenv";
+dotenv.config();
 
-const blockchainProvider = new BlockfrostProvider("preprod4XNNKV7AtEG8EjLc0kDwdIVoIVLx1x3F");
+const blockfrostId = process.env.BLOCKFROST_ID!;
+const blockchainProvider = new BlockfrostProvider(blockfrostId);
 
+const walletPassphrase = process.env.WALLET_PASSPHRASE!;
 const wallet = new MeshWallet({
     networkId: 0,
     fetcher: blockchainProvider,
     submitter: blockchainProvider,
     key: {
         type: "mnemonic",
-        words: [
-            "wallets_mnemonic",
-        ],
+        words: walletPassphrase.split(' ')
     },
 });
 
@@ -64,69 +66,69 @@ const txBuilder = new MeshTxBuilder({
 });
 
 
-const alwaysUtxos = await blockchainProvider.fetchUTxOs('5fffd05e3b4d2c25a68e3d9a6a0e3bf47b13e0f94e6961bda8a2d4741401c42a');
-const alwaysUtxo = alwaysUtxos[0];
-const onetimeUtxos = await blockchainProvider.fetchUTxOs('f83c70521872a57e5a7942b679442ab79c739ae44b7440174b7702afa4ad42fe');
-const onetimeUtxo = onetimeUtxos[0];
-
-const unsignedTx = await txBuilder
-    .txIn(
-        alwaysUtxo.input.txHash,
-        alwaysUtxo.input.outputIndex,
-        alwaysUtxo.output.amount,
-        alwaysUtxo.output.address,
-    )
-    .txIn(
-        onetimeUtxo.input.txHash,
-        onetimeUtxo.input.outputIndex,
-        onetimeUtxo.output.amount,
-        onetimeUtxo.output.address,
-    )
-    .mintPlutusScriptV3()
-    .mint("-1", scriptPolicy, tokenNameHex)
-    .mintingScript(parameterizedScript)
-    .mintRedeemerValue(mConStr0([]))
-    .mintPlutusScriptV3()
-    .mint("-1", scriptPolicy, tokenNameHex1)
-    .mintingScript(parameterizedScript)
-    .mintRedeemerValue(mConStr0([]))
-    .mintPlutusScriptV3()
-    .mint("1", scriptPolicy, tokenNameHex2)
-    .mintingScript(parameterizedScript)
-    .mintRedeemerValue(mConStr0([]))
-    .changeAddress(address)
-    .txInCollateral(
-        collateral.input.txHash,
-        collateral.input.outputIndex,
-        collateral.output.amount,
-        collateral.output.address,
-    )
-    .selectUtxosFrom(utxos)
-    // .requiredSignerHash(vk)
-    .complete();
+// const alwaysUtxos = await blockchainProvider.fetchUTxOs('5fffd05e3b4d2c25a68e3d9a6a0e3bf47b13e0f94e6961bda8a2d4741401c42a');
+// const alwaysUtxo = alwaysUtxos[0];
+// const onetimeUtxos = await blockchainProvider.fetchUTxOs('f83c70521872a57e5a7942b679442ab79c739ae44b7440174b7702afa4ad42fe');
+// const onetimeUtxo = onetimeUtxos[0];
 
 // const unsignedTx = await txBuilder
-    // // .txIn(
-    // //     theUtxo.input.txHash,
-    // //     theUtxo.input.outputIndex,
-    // //     theUtxo.output.amount,
-    // //     theUtxo.output.address,
-    // // )
-    // .mintPlutusScriptV3()
-    // .mint("1", scriptPolicy, tokenNameHex2)
-    // .mintingScript(parameterizedScript)
-    // .mintRedeemerValue(mConStr0([]))
-    // .changeAddress(address)
-    // .txInCollateral(
-    //     collateral.input.txHash,
-    //     collateral.input.outputIndex,
-    //     collateral.output.amount,
-    //     collateral.output.address,
-    // )
-    // .selectUtxosFrom(remainingUtxos)
-    // .requiredSignerHash(vk)
-    // .complete();
-const signedTx = await wallet.signTx(unsignedTx);
-const txHash = await wallet.submitTx(signedTx);
+//     .txIn(
+//         alwaysUtxo.input.txHash,
+//         alwaysUtxo.input.outputIndex,
+//         alwaysUtxo.output.amount,
+//         alwaysUtxo.output.address,
+//     )
+//     .txIn(
+//         onetimeUtxo.input.txHash,
+//         onetimeUtxo.input.outputIndex,
+//         onetimeUtxo.output.amount,
+//         onetimeUtxo.output.address,
+//     )
+//     .mintPlutusScriptV3()
+//     .mint("-1", scriptPolicy, tokenNameHex)
+//     .mintingScript(parameterizedScript)
+//     .mintRedeemerValue(mConStr0([]))
+//     .mintPlutusScriptV3()
+//     .mint("-1", scriptPolicy, tokenNameHex1)
+//     .mintingScript(parameterizedScript)
+//     .mintRedeemerValue(mConStr0([]))
+//     .mintPlutusScriptV3()
+//     .mint("1", scriptPolicy, tokenNameHex2)
+//     .mintingScript(parameterizedScript)
+//     .mintRedeemerValue(mConStr0([]))
+//     .changeAddress(address)
+//     .txInCollateral(
+//         collateral.input.txHash,
+//         collateral.input.outputIndex,
+//         collateral.output.amount,
+//         collateral.output.address,
+//     )
+//     .selectUtxosFrom(utxos)
+//     // .requiredSignerHash(vk)
+//     .complete();
 
-console.log(`\ntx hash: ${txHash}\n`);
+// const unsignedTx = await txBuilder
+//     .txIn(
+//         theUtxo.input.txHash,
+//         theUtxo.input.outputIndex,
+//         theUtxo.output.amount,
+//         theUtxo.output.address,
+//     )
+//     .mintPlutusScriptV3()
+//     .mint("1", scriptPolicy, tokenNameHex2)
+//     .mintingScript(parameterizedScript)
+//     .mintRedeemerValue(mConStr0([]))
+//     .changeAddress(address)
+//     .txInCollateral(
+//         collateral.input.txHash,
+//         collateral.input.outputIndex,
+//         collateral.output.amount,
+//         collateral.output.address,
+//     )
+//     .selectUtxosFrom(remainingUtxos)
+//     .requiredSignerHash(vk)
+//     .complete();
+// const signedTx = await wallet.signTx(unsignedTx);
+// const txHash = await wallet.submitTx(signedTx);
+
+// console.log(`\ntx hash: ${txHash}\n`);
